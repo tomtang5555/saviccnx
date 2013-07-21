b73b3a6b0ac29df6e58e0b76b77a09e78b421927
Tang Tang <thomastangatca@gmail.com>
2013-07-21 17:26:46 -0400

    updated namelist creation script for new experiments

--- onl_ccnx_utils/create_namelist.sh
+++ onl_ccnx_utils/create_namelist.sh
@@ -2,6 +2,7 @@
 
 OUTFILE="namelist"
 PREFIX="ccnx:/gen"
+START_INDEX=0
 ENTRY_PER_NODE=100
 NUM_NODE=0
 SUFFIX=""
@@ -19,23 +20,26 @@ if [[ -n "$4" ]]; then
 	NUM_NODE=$4
 fi
 if [[ -n "$5" ]]; then
-	SUFFIX=$5
+	START_INDEX=$5
+fi
+if [[ -n "$6" ]]; then
+	SUFFIX=$6
 fi
 
 # echo $PREFIX > $OUTFILE
 :>$OUTFILE
 
 if [[ $NUM_NODE -eq 0 ]]
-	then
-	for (( i=0; i<$ENTRY_PER_NODE; i++ ))
+then
+	for (( i=$START_INDEX; i<($START_INDEX + $ENTRY_PER_NODE); i++ ))
 	do
 		echo "$PREFIX/$i/$SUFFIX" >> $OUTFILE
 	done
 	
-	else
+else
 	for (( i=0; i<$NUM_NODE; i++ ))
 	do
-		for (( j=0; j<$ENTRY_PER_NODE; j++ ))
+		for (( j=$START_INDEX; j<($START_INDEX + $ENTRY_PER_NODE); j++ ))
 		do
 			echo "$PREFIX/$i/$j/$SUFFIX" >> $OUTFILE
 		done
